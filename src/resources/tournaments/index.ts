48fb5b459b401797c841df720f71ab661dabb67f
Dimashey <francisdossantos7@gmail.com>
2021-10-17 00:29:54 +0300

    create tournaments page

--- src/resources/tournaments/index.ts
+++ src/resources/tournaments/index.ts
@@ -1,6 +1,6 @@
 import { request } from 'request/index';
 import { TournamentDto } from 'models/tournaments';
 
-export const getTournaments = (): Promise<TournamentDto[]> => {
-  return request.get('tournaments');
+export const getTournaments = () => {
+  return request.get<TournamentDto[]>('tournaments');
 };
